a944de368af288948e491bb6a7b89ecf5498a53d
Guy Peled <guy.p@taboola.com>
2018-12-16 15:35:16 +0200

    init

--- myfirebase/index.js
+++ myfirebase/index.js
@@ -1,1 +1,21 @@
-import * as firebase from 'firebase';
+var firebase = require('firebase');
+
+
+var app = firebase.initializeApp({
+    apiKey: "AIzaSyDZZbKWz5B2ofJAkbI_jw2u51aPdNnNmO0",
+    authDomain: "avocado-backend.firebaseapp.com",
+    databaseURL: "https://avocado-backend.firebaseio.com",
+    projectId: "avocado-backend",
+    storageBucket: "avocado-backend.appspot.com",
+    messagingSenderId: "219538454820"
+  });
+
+  function writeUserData(userId, name, email, imageUrl) {
+    firebase.database().ref('users/' + userId).set({
+      username: name,
+      email: email,
+      profile_picture : imageUrl
+    });
+  }
+
+  writeUserData("test1", "test1", "test@gmail.com", "test.png");
