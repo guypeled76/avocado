08a4093ff80cd8a6477a186901adc40ed42e6e3e
Guy Peled <guy.p@taboola.com>
2018-12-22 16:40:40 +0200

    Add chat

--- avocado-client/src/components/views/chat/Chat.tsx
+++ avocado-client/src/components/views/chat/Chat.tsx
@@ -1,31 +1,66 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, ViewStyle } from 'react-native';
+import { GiftedChat, IMessage } from 'react-native-gifted-chat';
 
-
-interface ChatItem {
-
-}
 
 interface State {
-    items : ChatItem[];
+    messages: IMessage[];
 }
 
-interface Props {}
+interface Props { }
 
 export default class Chat extends React.Component<Props, State> {
-    public state: State = { 
-        items : []
+    public state: State = {
+        messages: []
     };
 
-    constructor(props : Props) {
+    constructor(props: Props) {
         super(props);
 
+
+    }
+
+
+    componentWillMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: 'https://placeimg.com/140/140/any',
+                    },
+                },
+            ],
+        })
+    }
+
+    onSend(messages : IMessage[]) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }))
+    }
+
+    render() {
+        return (
+            <GiftedChat
+                imageStyle={style}
+                messages={this.state.messages}
+                onSend={messages => this.onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+            />
+        )
+    }
+
+
+
+}
+
+const style : ViewStyle = {
         
-    }
-
-    public render() {
-
-        return <Text>Chat</Text>;
-    }
-
 }
