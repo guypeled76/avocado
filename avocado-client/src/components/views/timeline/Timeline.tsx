1e3f0f1723fde8aff74b4af94ef88d4b170abe06
Guy Peled <guy.p@taboola.com>
2018-12-23 22:32:10 +0200

    Add timeline component

--- avocado-client/src/components/views/timeline/Timeline.tsx
+++ avocado-client/src/components/views/timeline/Timeline.tsx
@@ -14,6 +14,10 @@ const data : { time: string; title: string; description: string; }[]= [
     {time: '10:45', title: 'Event 2', description: 'Event 2 Description'},
     {time: '12:00', title: 'Event 3', description: 'Event 3 Description'},
     {time: '14:00', title: 'Event 4', description: 'Event 4 Description'},
+    {time: '09:00', title: 'Event 1', description: 'Event 1 Description'},
+    {time: '10:45', title: 'Event 2', description: 'Event 2 Description'},
+    {time: '12:00', title: 'Event 3', description: 'Event 3 Description'},
+    {time: '14:00', title: 'Event 4', description: 'Event 4 Description'},
     {time: '16:30', title: 'Event 5', description: 'Event 5 Description'}
   ];
 
