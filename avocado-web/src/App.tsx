e0a48a76f8d792aa77c6aabff85ab5c6fc8d81b1
Guy Peled <guy.p@taboola.com>
2018-12-28 14:53:38 +0200

    avocado-web

--- avocado-web/src/App.tsx
+++ avocado-web/src/App.tsx
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const logo = require('./logo.svg');
+
+class App extends Component<{},{}> {
+
+  
+
   render() {
     return (
       <div className="App">
