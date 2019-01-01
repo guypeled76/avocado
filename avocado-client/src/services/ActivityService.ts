c19db2f0a28e4f119732ab99962ca03f88cddbcc
Guy Peled <guy.p@taboola.com>
2019-01-01 08:44:11 +0200

    Add log activities

--- avocado-client/src/services/ActivityService.ts
+++ avocado-client/src/services/ActivityService.ts
@@ -1,17 +1,31 @@
 import { db, field } from 'services';
-import { UserInfo } from 'avocado-common';
+import { UserInfo, ActivityInfo } from 'avocado-common';
 
 export class ActivityService {
 
     public static registerLogin(user:UserInfo) {
+        const timestamp = field.serverTimestamp();
+
         db.collection("profiles").doc(user.uid).set({
             displayName:user.displayName,
             email: user.email,
             uid: user.uid,
             phoneNumber: user.phoneNumber,
             photoURL: user.photoURL,
-            lastLogin:  field.serverTimestamp()
+            lastLogin:  timestamp,
+            lastActivity: timestamp
         });        
+    }
+
+    public static registerActivity(user:UserInfo, activity:ActivityInfo) {
+        const batch = db.batch();
+        const timestamp = field.serverTimestamp();
+        batch.update(db.collection("profiles").doc(user.uid), {lastActivity: timestamp});
+        batch.set(db.collection("profiles").doc(user.uid).collection("activities").doc(), {
+            ...activity,
+            timestamp:timestamp
+        });
+        batch.commit();
     }
 
 }
