2a6b77a789a862fe7d07555c00a5fdff9ec5fed6
Guy Peled <guy.p@taboola.com>
2019-01-07 09:42:28 +0200

    Initialize google services.

---
+++ avocado_client/lib/splash.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  SplashPage({Key key}) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        child: Center(
+            child: RaisedButton(
+              padding: const EdgeInsets.all(8.0),
+              textColor: Colors.white,
+              color: Colors.blue,
+              child: new Text("Splash"),
+            )
+        )
+    );
+  }
+}
