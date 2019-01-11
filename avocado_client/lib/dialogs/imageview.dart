52b8b3ff333e32e2fd95c62ada7aad3bf363d8fa
Guy Peled <guy.p@taboola.com>
2019-01-11 21:48:34 +0200

    photo editing

--- avocado_client/lib/dialogs/imageview.dart
+++ avocado_client/lib/dialogs/imageview.dart
@@ -23,7 +23,36 @@ class ImageViewDialog extends StatelessWidget {
         body: PhotoView(
             imageProvider: NetworkImage(this.imageInfo.image),
           ),
+        bottomNavigationBar: Column(
+          mainAxisSize:MainAxisSize.min,
+          children: <Widget>[
+            Divider(color: Colors.white,),
+            Container(
+              padding: EdgeInsets.only(right: 4,left:4),
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  _button(context, Icons.thumb_up, "Like"),
+                  _button(context, Icons.comment, "Comments"),
+                  _button(context, Icons.share, "Share"),
+                ])),
+            Container(height: 15,)
+          ],
+        )
     );
+  }
+
+  Widget _button(BuildContext context, IconData icon, String text) {
+    return FlatButton(onPressed: () => {},
+        color: Colors.transparent,
+        padding: EdgeInsets.all(0),
+        child: Row( // Replace with a Row for horizontal icon + text
+          children: <Widget>[
+            Icon(icon, color: Colors.white),
+            Container(width: 5,),
+            Text(text, style:TextStyle(color:Colors.white))
+          ],
+        ));
   }
 
   static void show(BuildContext context, ImageContentInfo imageInfo) {
