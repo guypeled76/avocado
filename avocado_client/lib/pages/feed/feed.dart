112fb74df48809fa0c4e5ece7d2c9c4074874f43
Guy Peled <guy.p@taboola.com>
2019-01-15 21:52:32 +0200

    sliver

--- avocado_client/lib/pages/feed/feed.dart
+++ avocado_client/lib/pages/feed/feed.dart
@@ -7,36 +7,36 @@ import 'stories.dart';
 class FeedPage extends StatelessWidget {
   FeedPage({Key key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(0),
-    child:
+        child: CustomScrollView(slivers: <Widget>[
+      SliverList(
+          delegate: SliverChildListDelegate(
+        [StoriesWidget()],
+      )),
       StreamBuilder<List<PostInfo>>(
           stream: loadPosts(),
           builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if(snapshot.hasError) {
-                return new Text(snapshot.error.toString());
-              }
-              return ListView.builder(
-                padding: EdgeInsets.all(0),
-                  itemCount: snapshot.data.length+1,
-                  itemBuilder: (context, index) {
-                    if(index==0) return StoriesWidget();
-                    return createPost(snapshot.data[index-1], null);
-                  });
-            } else {
-              return Text("Loading");
+            if (snapshot.hasError) {
+              return SliverFillRemaining(
+                  child: new Text(snapshot.error.toString()));
             }
+            return SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return createPost(snapshot.data[index], null);
+                },
+                childCount: snapshot.hasData ? snapshot.data.length : 0,
+              ),
+            );
           })
-    );
+    ]));
   }
 
   Widget createPost(PostInfo post, UserInfo user) {
     return Post(
-      image:post.image,
+      image: post.image,
       avatar: post.user.image,
       time: post.date,
       comments: "2",
