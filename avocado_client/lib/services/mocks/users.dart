bcbfdbc2932a00bb7f9c3bcbea5eef8dddb49130
Guy Peled <guy.p@taboola.com>
2019-01-15 11:59:39 +0200

    Remove redundant files

--- avocado_client/lib/services/mocks/users.dart
+++
@@ -1,9 +0,0 @@
-
-
-import 'package:avocado_client/data/data.dart';
-
-final user1 = UserInfo(
-    key :"u1",
-    name:"Albert Einstein",
-    image: "https://cdn.insidetheperimeter.ca/wp-content/uploads/2015/11/Albert_einstein_by_zuzahin-d5pcbug-WikiCommons-768x706.jpg"
-);
--- avocado_client/lib/services/posts.dart
+++
@@ -1,9 +0,0 @@
-
-
-import 'package:avocado_client/data/data.dart';
-import 'mocks/posts.dart';
-
-Stream<List<PostInfo>>  loadPosts  () async* {
-
-  yield posts;
-}
