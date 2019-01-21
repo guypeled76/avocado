1d271ab46adc6ef69e100492ad069fa383bc0b8b
Guy Peled <guy.p@taboola.com>
2019-01-21 21:18:42 +0200

    add support for web

--- avocado_web/web/main.dart
+++ avocado_web/web/main.dart
@@ -1,6 +1,14 @@
 import 'package:angular/angular.dart';
 import 'package:avocado_web/app_component.template.dart' as ng;
+import 'package:firebase/firebase.dart';
 
 void main() {
+  initializeApp(
+      apiKey: "AIzaSyDZZbKWz5B2ofJAkbI_jw2u51aPdNnNmO0",
+      authDomain: "avocado-backend.firebaseapp.com",
+      databaseURL: "https://avocado-backend.firebaseio.com",
+      projectId: "avocado-backend",
+      storageBucket: "avocado-backend.appspot.com");
+
   runApp(ng.AppComponentNgFactory);
 }
