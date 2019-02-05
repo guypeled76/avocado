30ea82959778b3b307d349cadae88f27ef4d2efc
Guy Peled <guy.p@taboola.com>
2019-02-05 21:32:39 +0200

    redux

--- avocado_common/lib/src/services/store.dart
+++ avocado_common/lib/src/services/store.dart
@@ -1,7 +1,30 @@
+import 'package:avocado_common/common.dart';
+import 'package:built_redux/built_redux.dart';
+import 'package:built_redux_rx/built_redux_rx.dart';
+
 import 'container.dart';
 
 abstract class StoreService implements ServiceContainer {
+  Store<AppState, AppStateBuilder, AppActions> _store = null;
+
+  StoreService() {
+    _store = new Store<AppState, AppStateBuilder, AppActions>(
+        createAppReducer(this), // build returns a reducer function
+        new AppState(),
+        new AppActions(),
+        middleware:
+          <Middleware<AppState, AppStateBuilder, dynamic>>[
+            createEpicMiddleware<AppState, AppStateBuilder, ReduxActions>([]),
+          ]
+        );
+  }
 
 
-  StoreService();
+  Stream<AppState> get state {
+    return this._store.nextState;
+  }
+
+  AppActions get actions {
+    return this._store.actions;
+  }
 }
