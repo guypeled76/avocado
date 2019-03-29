f8bf485221835228a6452467a10bd25b086df4aa
Guy Peled <guy.p@taboola.com>
2019-03-29 20:11:07 +0300

    Add avocado go server

---
+++ avocado_server/dal/firebase/firebase.go
@@ -0,0 +1,62 @@
+package firebase
+
+import (
+	"firebase.google.com/go"
+	"firebase.google.com/go/auth"
+	"firebase.google.com/go/db"
+	"golang.org/x/net/context"
+	"google.golang.org/api/option"
+	"log"
+	"sync"
+)
+
+type DBConnection struct {
+	Conn    *db.Client
+	App     *firebase.App
+	Context context.Context
+	Auth    *auth.Client
+}
+
+var instance *DBConnection
+var once sync.Once
+
+// BEWARE: Singleton method to get DB connection parameters
+func Connect() *DBConnection {
+	once.Do(func() {
+		context := context.Background()
+
+		// set configuration and options
+		conf := &firebase.Config{
+			DatabaseURL:      "https://avocado-backend.firebaseio.com",
+			ProjectID:        "avocado-backend",
+			ServiceAccountID: "",
+			StorageBucket:    "avocado-backend.appspot.com",
+		}
+
+		opt := option.WithCredentialsFile("serviceAccountKey.json")
+
+		// Create firebase app and db client variable and export
+		firebaseApp, err := firebase.NewApp(context, conf, opt)
+		if err != nil {
+			log.Fatalf("error initializing app: %v\n", err)
+		}
+
+		client, err := firebaseApp.Database(context)
+		if err != nil {
+			log.Fatalf("Error initializing database client:", err)
+		}
+
+		firebaseAuth, err := firebaseApp.Auth(context)
+		if err != nil {
+			log.Fatalf("Error initializing database client:", err)
+		}
+
+		instance = &DBConnection{
+			Context: context,
+			Conn:    client,
+			App:     firebaseApp,
+			Auth:    firebaseAuth,
+		}
+	})
+	return instance
+}
---
+++ avocado_server/go.mod
@@ -0,0 +1,11 @@
+module github.com/gremlinsapps/avocado_server
+
+require (
+	firebase.google.com/go v3.7.0+incompatible
+	github.com/99designs/gqlgen v0.8.2
+	github.com/golang/mock v1.2.0 // indirect
+	github.com/googleapis/gax-go v2.0.2+incompatible // indirect
+	github.com/vektah/gqlparser v1.1.2
+	golang.org/x/net v0.0.0-20190311183353-d8887717615a
+	google.golang.org/api v0.3.0
+)
