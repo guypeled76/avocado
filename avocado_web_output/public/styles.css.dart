e57002b04b478acfe04cebc30a1cdc20cc994aad
Guy Peled <guy.p@taboola.com>
2019-06-24 14:35:22 +0300

    remove client code

--- avocado_web_output/public/styles.css.dart
+++
@@ -1,4 +0,0 @@
-library asset_avocado_web_web_styles.css.dart;
-
-
-final List<dynamic> styles = ['@import url(https://fonts.googleapis.com/css?family=Roboto);\n@import url(https://fonts.googleapis.com/css?family=Material+Icons);\n\nbody, html {\n  height: 100%;\n  margin: 0px;\n  padding: 0px;\n}\n\n* {\n  font-family: Roboto, Helvetica, Arial, sans-serif;\n}\n'];
--- avocado_web_output/public/styles.css.shim.dart
+++
@@ -1,4 +0,0 @@
-library asset_avocado_web_web_styles.css.shim.dart;
-
-
-final List<dynamic> styles = ['@import url(https://fonts.googleapis.com/css?family=Roboto); @import url(https://fonts.googleapis.com/css?family=Material+Icons);body._ngcontent-%ID%,html._ngcontent-%ID%{height:100%;margin:0px;padding:0px}*._ngcontent-%ID%{font-family:Roboto,Helvetica,Arial,sans-serif}'];
