0ef7b016d97b0a5944562c056245d7c046b63992
Guy Peled <guy.p@taboola.com>
2019-07-03 16:15:28 +0300

    app engine functions

--- avocado_functions/app.go
+++ avocado_functions/app.go
@@ -14,12 +14,12 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
 		Message string `json:"message"`
 	}
 	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		fmt.Fprint(w, "Hello World git 1111!")
+		fmt.Fprint(w, "Hello World git!")
 		return
 	}
 	if d.Message == "" {
-		fmt.Fprint(w, "Hello World git 1111!")
+		fmt.Fprint(w, "Hello World git!")
 		return
 	}
-	fmt.Fprint(w, fmt.Sprintf("Message git %s 111", html.EscapeString(d.Message)))
+	fmt.Fprint(w, fmt.Sprintf("Message git %s", html.EscapeString(d.Message)))
 }
