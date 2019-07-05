de9acc36537e844dda026a1e3e4ec76cb7afaa7d
Guy Peled <guy.p@taboola.com>
2019-07-05 18:17:54 +0300

    client

--- avocado_common/lib/src/blocs/SearchBLoC.dart
+++ avocado_common/lib/src/blocs/SearchBLoC.dart
@@ -5,13 +5,13 @@ import 'package:rxdart/rxdart.dart';
 
 class SearchBLoC extends BaseBLoC{
 
-  BehaviorSubject<String> _query = new BehaviorSubject(seedValue: "");
+  BehaviorSubject<String> _query = new BehaviorSubject.seeded("");
 
   Sink<String> get query {
     return _query;
   }
 
   Stream<List<SearchInfo>> get results {
-    return _query.debounce(Duration(microseconds: 50)).asyncExpand(loadSearch);
+    return null;//_query.debounce(Duration(microseconds: 50)).asyncExpand(loadSearch);
   }
 }
