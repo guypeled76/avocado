e8e03fd286d3871e108d1ffd55a5e5745c3f6fde
Guy Peled <guy.p@taboola.com>
2019-07-07 21:37:19 +0300

    Enhance service locator pattern

--- avocado_client/android/app/src/main/java/com/gremlinsapps/avocado_client/MainActivity.java
+++ avocado_client/android/app/src/main/java/com/gremlinsapps/avocado_client/MainActivity.java
@@ -1,6 +1,9 @@
 package com.gremlinsapps.avocado_client;
 
 import android.os.Bundle;
+
+import com.facebook.FacebookSdk;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
@@ -8,6 +11,9 @@ public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    FacebookSdk.setApplicationId("2289351617806667");
+    FacebookSdk.sdkInitialize(getApplicationContext());
     GeneratedPluginRegistrant.registerWith(this);
   }
 }
